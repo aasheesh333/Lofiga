5550938a0fd66070c858974993fc85c788587cfa
Unspoken Productions <apple@Apples-MacBook-Air.local>
2026-02-01 20:43:17 +0530

    fix: use npm overrides to alias worklets to reanimated

--- react-native-worklets.js
+++
@@ -1,4 +0,0 @@
-// Polyfill for react-native-worklets
-// This file provides compatibility shims for packages that might reference
-// the deprecated react-native-worklets package
-module.exports = require('react-native-reanimated');
