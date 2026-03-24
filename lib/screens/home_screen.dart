cbe7843e790dc831e7bf991b0db77cf9c7ffb3bf
Aasheesh Katheriya <aasheeshkatheriya@gmail.com>
2026-03-24 04:44:37 +0000

    fix(permissions): add Android 13+ audio permissions fallback

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -98,20 +98,27 @@ class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
       
       if (Platform.isAndroid) {
         // 1. Check all forms of storage permission
-        if (await Permission.manageExternalStorage.isGranted || 
+        if (await Permission.audio.isGranted || 
+            await Permission.manageExternalStorage.isGranted || 
             await Permission.storage.isGranted) {
           permissionStatus = true;
         } else {
-          // 2. Request All Storage (manageExternalStorage) for Android 11+
-          // This will open a settings toggle page on Android 11+ and might do a system popup on Android 10-
-          final manageStatus = await Permission.manageExternalStorage.request();
-          if (manageStatus.isGranted) {
+          // 2. Request Audio Permission for Android 13+
+          final audioStatus = await Permission.audio.request();
+          if (audioStatus.isGranted) {
             permissionStatus = true;
           } else {
-            // 3. Fallback to normal storage request (for Android 10 and below, or emulators)
-            final storageStatus = await Permission.storage.request();
-            if (storageStatus.isGranted) {
+            // 3. Request All Storage (manageExternalStorage) for Android 11-12
+            // This will open a settings toggle page on Android 11+ and might do a system popup on Android 10-
+            final manageStatus = await Permission.manageExternalStorage.request();
+            if (manageStatus.isGranted) {
               permissionStatus = true;
+            } else {
+              // 4. Fallback to normal storage request (for Android 10 and below, or emulators)
+              final storageStatus = await Permission.storage.request();
+              if (storageStatus.isGranted) {
+                permissionStatus = true;
+              }
             }
           }
         }
