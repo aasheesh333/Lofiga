6359da794d1ab1bcc882dfaa1a2754c44408ee10
root <root@localhost.localdomain>
2026-05-13 06:48:20 +0000

    fix: add missing BorderStroke import in CommonComponents.kt

--- Lofiga/native-android/app/src/main/java/com/dhanuk/lofiga/ui/components/CommonComponents.kt
+++ Lofiga/native-android/app/src/main/java/com/dhanuk/lofiga/ui/components/CommonComponents.kt
@@ -1,6 +1,7 @@
 package com.dhanuk.lofiga.ui.components
 
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
