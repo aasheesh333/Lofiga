343c984ad1efcfacf896c28f7dba501b69b4a822
Lofiga Dev <dev@lofiga.app>
2026-05-29 17:38:58 +0000

    fix: simplify OneSignal init to essentials, pass AppID to initWithContext

--- Lofiga/native-android/app/src/main/java/com/dhanuk/lofiga/LofigaApplication.kt
+++ Lofiga/native-android/app/src/main/java/com/dhanuk/lofiga/LofigaApplication.kt
@@ -1,15 +1,10 @@
 package com.dhanuk.lofiga
 
 import android.app.Application
-import android.util.Log
 import com.dhanuk.lofiga.ads.AdManager
 import com.dhanuk.lofiga.media.MediaNotificationManager
 import com.dhanuk.lofiga.media.MediaSessionManager
 import com.onesignal.OneSignal
-import com.onesignal.notifications.INotificationClickEvent
-import com.onesignal.notifications.INotificationClickListener
-import com.onesignal.user.IPushSubscriptionObserver
-import com.onesignal.user.PushSubscriptionChangedState
 
 class LofigaApplication : Application() {
 
@@ -22,18 +17,6 @@ class LofigaApplication : Application() {
         super.onCreate()
 
         OneSignal.initWithContext(this, BuildConfig.ONESIGNAL_APP_ID)
-
-        OneSignal.Notifications.addClickListener(object : INotificationClickListener {
-            override fun onClick(event: INotificationClickEvent) {
-                Log.d("OneSignal", "Notification clicked: ${event.notification.title}")
-            }
-        })
-
-        OneSignal.User.pushSubscription.addObserver(object : IPushSubscriptionObserver {
-            override fun onPushSubscriptionChange(state: PushSubscriptionChangedState) {
-                Log.d("OneSignal", "Push subscription: optedIn=${state.current.optedIn}, token=${state.current.token}")
-            }
-        })
 
         mediaSessionManager = MediaSessionManager(this)
         mediaNotificationManager = MediaNotificationManager(this)
